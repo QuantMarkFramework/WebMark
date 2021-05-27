caa55fb10dbdc3d3b1522855271d3d639cd62e36
Bo Granlund <bo.granlund@helsinki.fi>
2021-05-27 20:21:36 +0300

    [test] Change API usage v.2

--- manage.py
+++ manage.py
@@ -8,7 +8,6 @@ import dotenv
 
 def main():
     """Run administrative tasks."""
-    #dotenv.read_dotenv()
     dotenv.load_dotenv()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WebMark.settings')
     try:
