ac1bd97c67f95aa4f0c5b770da9dae8f7b96709a
Henri0088 <Henri.Sundquist@rokki.net>
2021-05-28 15:01:52 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,12 +3,12 @@
 import os
 import sys
 
-import dotenv
+from dotenv import load_dotenv
 
 
 def main():
     """Run administrative tasks."""
-    #dotenv.load_dotenv()
+    load_dotenv()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WebMark.settings')
     try:
         from django.core.management import execute_from_command_line
